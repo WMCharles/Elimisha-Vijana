7c48e2a7ea417e6efa2274c86a15a0ba88c998fe
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-07-20 15:23:18 +0300

    declare variables

---
+++ assets/js/login.js
@@ -0,0 +1,6 @@
+const form=document.getElementById('form');
+const firstName = document.getElementById('firstname');
+const username=document.getElementById('username');
+const password =document.getElementById('password');
+const confirmPassword=document.getElementById('password2');
+
