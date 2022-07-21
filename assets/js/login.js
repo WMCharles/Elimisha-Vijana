ee6d96fe39c8539d3279199af6b76344e1d7115f
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-21 11:52:16 +0300

    modify login js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,23 @@
-const form=document.getElementById('form');
-const firstName = document.getElementById('firstname');
-const username=document.getElementById('username');
-const password =document.getElementById('password');
-const confirmPassword=document.getElementById('password2');
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    validateInputs()
-})
+
+    validateInputs();
+});
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
 
 const setError = (element, message) => {
     const inputControl = element.parentElement;
@@ -18,47 +28,28 @@ const setError = (element, message) => {
     inputControl.classList.remove('success')
 }
 
-const setCorrect = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const firstNameValue = firstName.value.trim();
+    const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
-    const password2Value = confirmPassword.value.trim();
 
-    if(usernameValue === '') {
-        setError(username, 'Username is required!');
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
     } else {
-        setCorrect (username);
-    }
-
-    if(firstNameValue === ''){
-        setError(firstName, 'First name is required!')
-    } 
-    else {
-        setCorrect(firstName)
+        setSuccess(email);
     }
 
     if(passwordValue === '') {
-        setError(password, 'Password is required!');
+        setError(password, 'Password is required');
     } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character!')
+        setError(password, 'Password must be at least 8 character.')
     } else {
         setSuccess(password);
-    }
-
-    if(password2Value === '') {
-        setError(confirmPassword, 'Please confirm your password!');
-    } else if (password2Value !== passwordValue) {
-        setError(confirmPassword, "Passwords doesn't match!");
-    } else {
-        setSuccess(confirmPassword);
     }
 };
